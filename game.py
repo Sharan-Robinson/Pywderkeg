064ef38a425620bc703c1f7af7ea2bb0d86d3797
Sharan-Robinson <89362113+Sharan-Robinson@users.noreply.github.com>
2022-07-30 20:49:55 -0700

    Add files via upload
    Updated Sql integration

--- game.py
+++ game.py
@@ -2,24 +2,55 @@ import sqlite3
 
 
 class game():
-    def __int__(self, nation, savedbefore=False, timeelapsed=None):
+    def __int__(self, nation, savedbefore, timeelapsed):
         self.nation = nation
         self.savedbefore = savedbefore
         self.timeelapsed = timeelapsed
 
+    def startgame(self):
+        print("Please select a nation to play as!")
+        gamenation = input(
+            "1. Austria-Hungary\n"
+            "2.Serbia\n"
+            "3. Greece\n"
+            "4. Turkey\n"
+            "5. Bulgaria\n"
+        )
+
+        savegamename = input("What would you like to call this game?")
+        self.createsavegame(self.game())
+
+        
+
     def createnations(self, name):
-        austria = nation('austria', 50000, 50.0)
-        serbia = nation('Serbia', 10000, 13.0)
-        greece = nation('Greece', 15000, 5.0)
-        albania = nation('Albania', 5000, 3.1)
-        bulgaria = nation('Bulgaria', 10000, 10.0)
-        romania = nation('Romania', 20000, 5.0)
-        pass
+        austria = nation('Austria', 50000, 50)
+        serbia = nation('Serbia', 10000, 13)
+        greece = nation('Greece', 15000, 5)
+        albania = nation('Albania', 5000, 3)
+        bulgaria = nation('Bulgaria', 10000, 10)
+        romania = nation('Romania', 20000, 5)
+
 
     def createsavegame(self, savegamename):
-        conn = sqlite3.connect(f'{savegamename}.db')
+        conn = sqlite3.connect('savegame1.db')
 
-        pass
+        c = conn.cursor()
+
+        c.execute("""
+        CREATE TABLE IF NOT EXISTS savegame(
+        nation TEXT, 
+        armysize INT,
+        unrest INT
+        )
+        """)
+        c.execute("""
+        INSERT INTO savegame VALUES(
+        "Austria",
+        50000,
+        50
+        )""")
+        conn.commit()
+        conn.close()
 
 
 class nation():
